078998a62d0f0de1f92b63b02427bec6d0f9821e
henry <hu_lian_cn@163.com>
2022-07-16 14:19:18 +0800

    add java client

---
+++ gradle-docker-plugin-java/src/main/java/pub/techfun/docker/plugin/java/client/DockerCmdClient.java
@@ -0,0 +1,69 @@
+package pub.techfun.docker.plugin.java.client;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.async.ResultCallback;
+import com.github.dockerjava.api.model.BuildResponseItem;
+import com.github.dockerjava.api.model.PushResponseItem;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import org.gradle.api.logging.Logger;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author henry
+ */
+public class DockerCmdClient {
+
+    private static DockerClient client;
+
+
+    public static void buildImage(Logger logger, File folder){
+        DockerClient client = getClient();
+        CountDownLatch latch = new CountDownLatch(1);
+        ResultCallback<BuildResponseItem> handler = new DefaultCallBackHandler<>(
+                logger, latch
+        );
+        client.buildImageCmd(folder).exec(handler);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void pushImage(Logger logger, String name){
+        DockerClient client = getClient();
+        CountDownLatch latch = new CountDownLatch(1);
+        ResultCallback<PushResponseItem> handler = new DefaultCallBackHandler<>(
+                logger, latch
+        );
+        client.pushImageCmd(name).exec(handler);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static DockerClient getClient() {
+        if(client!=null){
+            return client;
+        }
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+        client = DockerClientImpl.getInstance(config, httpClient);
+        return client;
+    }
+}
--- gradle-docker-plugin-java/src/main/java/pub/techfun/docker/plugin/java/task/PushImageTask.java
+++ gradle-docker-plugin-java/src/main/java/pub/techfun/docker/plugin/java/task/PushImageTask.java
@@ -1,33 +1,33 @@
 package pub.techfun.docker.plugin.java.task;
 
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 import pub.techfun.docker.plugin.common.constants.Constants;
 import pub.techfun.docker.plugin.common.util.ImageNameUtil;
 import pub.techfun.docker.plugin.common.util.LogUtil;
 import pub.techfun.docker.plugin.common.util.PropertyUtil;
-import org.gradle.api.tasks.Exec;
+import pub.techfun.docker.plugin.java.client.DockerCmdClient;
 
 /**
  * @author henry
  */
-public class PushImageTask extends Exec {
+public class PushImageTask extends DefaultTask {
 
 	public static final String TASK_NAME = "pushImage";
 
 	public PushImageTask(){
 		dependsOn(getProject().getTasks().getByName(BuildImageTask.TASK_NAME));
 		setGroup(Constants.GROUP_NAME);
-		workingDir(super.getProject().getBuildDir().getPath() + Constants.DOCKER_FOLDER);
 	}
 
-	@Override
+	@TaskAction
 	protected void exec() {
 		if(PropertyUtil.hasRegistryHost(getProject())) {
 			String imageName = ImageNameUtil.getImageName(getProject());
 			LogUtil.logLifeCycle(super.getLogger(),"推送Docker镜像:"+imageName);
-			commandLine("docker", "push", imageName);
+			DockerCmdClient.pushImage(getLogger(), imageName);
 		}else{
-			commandLine("echo", "'没有设置远程仓库地址，不推送镜像！'");
+			LogUtil.logLifeCycle(super.getLogger(),"没有设置远程仓库地址，不推送镜像!");
 		}
-		super.exec();
 	}
 }
