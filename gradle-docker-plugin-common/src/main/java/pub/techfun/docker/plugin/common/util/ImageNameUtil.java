092f50260bf18de5a7b2a3b30c0db681f3256f96
henry <henrymetting@outlook.com>
2022-07-18 09:08:20 +0800

    add java client

--- gradle-docker-plugin-common/src/main/java/pub/techfun/docker/plugin/common/util/ImageNameUtil.java
+++ gradle-docker-plugin-common/src/main/java/pub/techfun/docker/plugin/common/util/ImageNameUtil.java
@@ -11,21 +11,12 @@ import java.nio.file.Paths;
  */
 public class ImageNameUtil {
 	public static String getImageName(Project project){
-		String imageName;
 		String gitVersion = "";
 		try {
 			gitVersion = Files.readString(Paths.get(project.getBuildDir().getPath(),"/docker/GitVersion"));
 		} catch (IOException ignore) {
 		}
-		if(gitVersion!=null){
-			gitVersion =  ":" + gitVersion.replace("\n","");
-		}
-		if(PropertyUtil.hasRegistryHost(project)){
-			imageName = PropertyUtil.getRegistryHost(project) + "/" + project.getName() + gitVersion;
-		}else{
-			imageName = project.getName() + gitVersion;
-		}
-		return imageName;
+		return getImageName(project, gitVersion);
 	}
 
 	public static String getImageName(Project project, String gitVersion){
