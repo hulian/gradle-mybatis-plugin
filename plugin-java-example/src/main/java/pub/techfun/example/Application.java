20fa64d2555a922f1914bb3a2d80e174e250a1d0
henry <hu_lian_cn@163.com>
2022-08-15 21:25:53 +0800

    init

--- plugin-java-example/src/main/java/pub/techfun/example/Application.java
+++
@@ -1,8 +0,0 @@
-package pub.techfun.example;
-
-public class Application {
-    public static void main(String[] args) {
-        System.out.printf("This is an example application!");
-        System.out.printf(System.getProperties().toString());
-    }
-}
