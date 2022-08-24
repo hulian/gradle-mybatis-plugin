fe213faf37d089dfa3b7bbc6e515d13e62d66569
henry <henrymetting@outlook.com>
2022-08-24 09:54:54 +0800

    init

--- gradle-mybatis-plugin-common/src/main/java/pub/techfun/mybatis/plugin/common/task/CreateConfigFileTask.java
+++ gradle-mybatis-plugin-common/src/main/java/pub/techfun/mybatis/plugin/common/task/CreateConfigFileTask.java
@@ -17,45 +17,50 @@ public class CreateConfigFileTask extends DefaultTask {
 
 	public static final String TASK_NAME = "createConfigFile";
 	public static String TYPE;
-	private final String configFrom;
-	private final String driverFrom;
+	private final String configBuildFolder;
+	private final String driverBuildFolder;
 	private final String defaultConfigFrom;
 
 	public CreateConfigFileTask(){
-		driverFrom = getProject().getBuildDir().getPath() +"/"+ Constants.DRIVER_FOLDER;
-		configFrom = getProject().getBuildDir().getPath() +"/"+ Constants.CONFIG_FOLDER;
+		driverBuildFolder = getProject().getBuildDir().getPath() +"/"+ Constants.DRIVER_FOLDER;
+		configBuildFolder = getProject().getBuildDir().getPath() +"/"+ Constants.CONFIG_FOLDER;
 		defaultConfigFrom = getProject().getProjectDir().getPath() +"/"+ Constants.CONFIG_FOLDER;
 		setGroup(Constants.GROUP_NAME);
 	}
 
 	@TaskAction
 	protected void copy() throws IOException {
-		var file = Paths.get(driverFrom);
-		if (!Files.exists(file)) {
-			LogUtil.logLifeCycle(getLogger(), "未发现driver目录,从classpath复制:" + driverFrom);
-			Files.createDirectories(file);
-			FileResourcesUtils.copy(getLogger(), Constants.CONFIG_FOLDER + "-driver", driverFrom);
+		// 复制驱动
+		var driverBuildPath = Paths.get(driverBuildFolder);
+		if (!Files.exists(driverBuildPath)) {
+			LogUtil.logLifeCycle(getLogger(), "未发现driver目录,从classpath复制:" + driverBuildFolder);
+			Files.createDirectories(driverBuildPath);
+			FileResourcesUtils.copy(getLogger(), Constants.CONFIG_FOLDER + "-driver", driverBuildFolder);
 		}
-		file = Paths.get(defaultConfigFrom);
-		if (Files.exists(file)) {
-			LogUtil.logLifeCycle(getLogger(), "有配置Config目录,从Config目录复制:" + configFrom);
-			Files.walkFileTree(file, new SimpleFileVisitor<>() {
+		// 复制配置文件
+		var defaultConfigPath = Paths.get(defaultConfigFrom);
+		var configBuildPath = Paths.get(configBuildFolder);
+		if (Files.exists(defaultConfigPath)) {
+			LogUtil.logLifeCycle(getLogger(), "有配置Config目录,从Config目录复制:" + defaultConfigFrom);
+			if (!Files.exists(configBuildPath)) {
+				Files.createDirectories(configBuildPath);
+			}
+			Files.walkFileTree(defaultConfigPath, new SimpleFileVisitor<>() {
 				@Override
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
 					Files.copy(
 							file,
-							Paths.get(configFrom, file.getFileName().toString()),
+							Paths.get(configBuildFolder, file.getFileName().toString()),
 							StandardCopyOption.REPLACE_EXISTING
 					);
 					return super.visitFile(file, attrs);
 				}
 			});
 		} else {
-			file = Paths.get(configFrom);
-			if (!Files.exists(file)) {
+			if (!Files.exists(configBuildPath)) {
 				LogUtil.logLifeCycle(getLogger(), "未配置Config目录,从classpath复制");
-				Files.createDirectories(file);
-				FileResourcesUtils.copy(getLogger(), Constants.CONFIG_FOLDER + "-" + TYPE, configFrom);
+				Files.createDirectories(configBuildPath);
+				FileResourcesUtils.copy(getLogger(), Constants.CONFIG_FOLDER + "-" + TYPE, configBuildFolder);
 			}
 		}
 	}
